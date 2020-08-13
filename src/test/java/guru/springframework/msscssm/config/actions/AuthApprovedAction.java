26f8b36ad65ade1a0990c7b42c433e7e07124c9a
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-14 01:35:29 +0200

    214

---
+++ src/test/java/guru/springframework/msscssm/config/actions/AuthApprovedAction.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscssm.config.actions;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jantonio on 14/08/2020.
+ */
+@Component
+public class AuthApprovedAction implements Action<PaymentState, PaymentEvent> {
+    @Override
+    public void execute(StateContext<PaymentState, PaymentEvent> context) {
+        System.out.println("Sending Notification of Auth APPROVED");
+    }
+}
